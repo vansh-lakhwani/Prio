958bfaa6c348d8d4ddad94bb71d61b1d08396c50
Vansh Lakhwani <lakhwanivansh@gmail.com>
2026-04-08 01:49:49 +0530

    Created Prio-v-1.5.26

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import { redirect } from "next/navigation";
 
-export default function LandingPage() {
+export default function LandingPage({
+  searchParams,
+}: {
+  searchParams: { code?: string };
+}) {
+  if (searchParams?.code) {
+    redirect(`/auth/callback?code=${searchParams.code}`);
+  }
   redirect("/login");
 }
